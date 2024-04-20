e6bda7145a1510ac6534e94c68193203d26fef29
Daniel Callisaya <danielj.callisayah@gmail.com>
2024-04-20 12:34:38 -0400

    Initial Commit

---
+++ testanimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testanimation
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 20/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testanimation/testanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  testanimationApp.swift
+//  testanimation
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct testanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
